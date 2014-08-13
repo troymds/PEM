1cde0d95511a9fd408825a6904a411b9c06b2edb
jch <jch@jchdeMac-mini.local>
2014-08-13 08:38:51 +0800

    Initial Commit

---
+++ PEM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PEM
+//
+//  Created by jch on 14-8-13.
+//  Copyright (c) 2014年 ___普尔摩___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "proAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([proAppDelegate class]));
+    }
+}
---
+++ PEM/proAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  proAppDelegate.h
+//  PEM
+//
+//  Created by jch on 14-8-13.
+//  Copyright (c) 2014年 ___普尔摩___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface proAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
